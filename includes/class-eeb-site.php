998cd9035bce115c3553bf92df20523980cf75d9
freelancephp <freelancephp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-20 16:52:17 +0000

    version 1.4.1
    git-svn-id: https://plugins.svn.wordpress.org/email-encoder-bundle/trunk@1184801 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/class-eeb-site.php
+++ includes/class-eeb-site.php
@@ -32,6 +32,7 @@ final class Eeb_Site extends Eeb_Admin {
         'mailto' => '/<a([^<>]*?)href=["\']mailto:(.*?)["\'](.*?)>(.*?)<\/a[\s+]*>/is',
         'email' => '/([A-Z0-9._-]+@[A-Z0-9][A-Z0-9.-]{0,61}[A-Z0-9]\.[A-Z.]{2,6})/is',
         'input' => '/<input([^>]*)value=["\'][\s+]*([A-Z0-9._-]+@[A-Z0-9][A-Z0-9.-]{0,61}[A-Z0-9]\.[A-Z.]{2,6})[\s+]*["\']([^>]*)>/is',
+        'class' => '/class=["\'](.*?)["\']/i',
     );
 
     /**
@@ -145,16 +146,20 @@ final class Eeb_Site extends Eeb_Admin {
      * WP head
      */
     public function wp_head() {
-            // add styling for encoding check message + icon
-            if ($this->is_admin_user && $this->options['show_encoded_check']) {
-                echo <<<CSS
-<style type="text/css">
-    a.encoded-check { opacity:0.5; position:absolute; text-decoration:none !important; font:10px Arial !important; margin-top:-3px; color:#629632; font-weight:bold; }
-    a.encoded-check:hover { opacity:1; cursor:help; }
-    a.encoded-check img { width:10px; height:10px; }
-</style>
-CSS;
-            }
+        $css = '';
+        $css .= '<style type="text/css" media="screen">';
+
+        // add styling for encoding check message + icon
+        if ($this->is_admin_user && $this->options['show_encoded_check']) {
+            $css .= 'a.encoded-check { opacity:0.5; position:absolute; text-decoration:none !important; font:10px Arial !important; margin-top:-3px; color:#629632; font-weight:bold; }';
+            $css .= 'a.encoded-check:hover { opacity:1; cursor:help; }';
+            $css .= 'a.encoded-check img { width:10px; height:10px; }';
+        }
+
+//        $css .= 'a[href^="mailto:"] .fa { margin-right:0.4em; }';
+        $css .= '</style>';
+
+        echo $css;
     }
 
     /* -------------------------------------------------------------------------
@@ -383,9 +388,18 @@ CSS;
             $email = $this->enc_html($email);
         }
 
-        $class = $this->options['class_name'];
         $extra_attrs = ' ' . trim($extra_attrs);
-        $mailto = '<a class="'. $class .'" href="mailto:' . $email . '"'. $extra_attrs . '>' . $display . '</a>';
+
+        // add class
+        if (preg_match($this->regexp_patterns['class'], $extra_attrs, $matches)) {
+            // class attribute set
+            $extra_attrs = str_replace($matches[0], sprintf('class="%s"', $matches[1] . ' ' . $this->options['class_name']), $extra_attrs);
+        } else {
+            // class attribute not set
+            $extra_attrs .= ' class="' . $this->options['class_name'] . '"';
+        }
+
+        $mailto = '<a href="mailto:' . $email . '"'. $extra_attrs . '>' . $display . '</a>';
 
         if ($method === 'enc_html') {
             // add visual check
--- readme.txt
+++ readme.txt
@@ -1,9 +1,9 @@
 === Email Encoder Bundle - Protect Email Address ===
 Contributors: freelancephp
 Tags: email address, protect, antispam, mailto, spambot, secure, e-mail, email, mail, obfuscate, encode, encoder, encrypt, hide, bot, crawl, spider, robots, spam, protection, harvest, harvesting, security
-Requires at least: 3.4.0
-Tested up to: 4.1.1
-Stable tag: 1.4.0
+Requires at least: 3.6.0
+Tested up to: 4.2.2
+Stable tag: 1.4.1
 
 Encode mailto links, email addresses, phone numbers and any text to hide them from (spam)bots. Mailto links will be protected automatically.
 
@@ -124,6 +124,9 @@ It's possible to filter all widgets by using the [Widget Logic Plugin](https://w
 * Title icon on Admin Options Page was made by [Jack Cai](http://www.doublejdesign.co.uk/)
 
 == Changelog ==
+
+= 1.4.1 =
+* Fixed [preserving classes on mailto links](https://wordpress.org/support/topic/preserve-link-classes)
 
 = 1.4.0 =
 * Fixed bug prefilled email address in input fields
